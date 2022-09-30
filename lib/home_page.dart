f189124fcc3a0c92594a4d7e745acc8f6d6730ce
jvbt3 <joaovitormartins077@gmail.com>
2022-09-30 11:41:55 -0300

    30/09/2022 v1

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/models/chat_model.dart';
+import 'package:flutter_application_1/models/conversas_model.dart';
 import 'package:whatsapp/whatsapp.dart';
 
 class HomePage extends StatefulWidget {
@@ -12,15 +14,110 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   Whatsapp whatsapp = Whatsapp();
 
-  List conversa = ["teste", "teste2"];
+  List<ConversasModel> conversa = [
+    ConversasModel(
+        nome: 'João Bosco',
+        ultimaMensagem: ':)',
+        status: 'Atendido!',
+        hora: const TimeOfDay(hour: 15, minute: 0),
+        chats: [
+          ChatModel(
+            mensagem: ':)',
+            hora: const TimeOfDay(hour: 15, minute: 0),
+          ),
+        ]),
+    ConversasModel(
+        nome: 'Naiara Azevedo',
+        ultimaMensagem: 'Quero Cantar!',
+        status: 'Em espera!',
+        hora: const TimeOfDay(hour: 15, minute: 0),
+        chats: [
+          ChatModel(
+            mensagem: 'Quero Cantar!',
+            hora: const TimeOfDay(hour: 15, minute: 0),
+          ),
+        ]),
+    ConversasModel(
+        nome: 'Wagner OfficialTi',
+        ultimaMensagem: 'Servidor caiu aqui ó...',
+        status: 'Em atendido!',
+        hora: const TimeOfDay(hour: 15, minute: 0),
+        chats: [
+          ChatModel(
+            mensagem: 'Servidor caiu aqui ó...',
+            hora: const TimeOfDay(hour: 15, minute: 0),
+          ),
+        ]),
+    ConversasModel(
+        nome: 'Marquin',
+        ultimaMensagem: ':like:',
+        status: 'Atendido!',
+        hora: const TimeOfDay(hour: 15, minute: 0),
+        chats: [
+          ChatModel(
+            mensagem: ':like:',
+            hora: const TimeOfDay(hour: 15, minute: 0),
+          ),
+        ]),
+    ConversasModel(
+        nome: 'Suporte4',
+        ultimaMensagem: ':)',
+        status: 'Atendido!',
+        hora: const TimeOfDay(hour: 15, minute: 0),
+        chats: [
+          ChatModel(
+            mensagem: 'Olá, tudo bem?',
+            hora: const TimeOfDay(hour: 15, minute: 0),
+          ),
+        ]),
+    ConversasModel(
+        nome: 'Desenv01',
+        ultimaMensagem: 'Boa Tarde! Preciso de ajuda...',
+        status: 'Atribuido!',
+        hora: const TimeOfDay(hour: 15, minute: 0),
+        chats: [
+          ChatModel(
+            mensagem: 'Olá, tudo bem? Preciso de ajuda...',
+            hora: const TimeOfDay(hour: 15, minute: 0),
+          ),
+        ]),
+    ConversasModel(
+        nome: 'joao2',
+        ultimaMensagem: ':)',
+        status: 'Atendido!',
+        hora: const TimeOfDay(hour: 15, minute: 0),
+        chats: [
+          ChatModel(
+            mensagem: 'Olá, tudo bem?',
+            hora: const TimeOfDay(hour: 15, minute: 0),
+          ),
+        ]),
+    ConversasModel(
+        nome: 'joao1',
+        ultimaMensagem: ':)',
+        status: 'Atendido!',
+        hora: const TimeOfDay(hour: 15, minute: 0),
+        chats: [
+          ChatModel(
+            mensagem: 'Olá, tudo bem?',
+            hora: const TimeOfDay(hour: 15, minute: 0),
+          ),
+        ]),
+  ];
+
+  bool chatAberto = false;
+
+  List chat = [];
 
   final mensagemController = TextEditingController();
+
+  int? conversasSelecionadas = 0;
 
   @override
   void initState() {
     whatsapp.setup(
         accessToken:
-            "EAAI3ZCh6uivIBACCUjirHqjOJgRFkfZA1suqXzbpTI23JpujUCRBtWN1i5Y3YNXkCT7B2VNdnTFylqXDuyxiKwyofTXjxlRFumzH6XQWAKMgwWZA6BSzQ51euf0iXgmeBRCne5EGipwZCVMZCugzdurTtsEHZAERwnj4ZBGcDiDM6bUFkZCSUgcDW5e5kBGCldMi833UCgWM6gZDZD",
+            "EAAI3ZCh6uivIBANKNvd9C4fxewwNIcDVvZBqdy0NJEZBfEZCwx3EhShMEpGJuGem9ZBOVBGm7HiC6qf4smlZAjCwFFkVnnGptyhZB74IdU8uL8s4daTujEzFBd59YgT3txBmdxxoZAffBmFWzqdmshoilCHuJ2nG9ZAXx4ZCG086xSoMw5KLmHB4adKOAXHO3jfd5DfZAGvijqQXMgNIalw8IIvFEDjomVMf9sZD",
         fromNumberId: 107930172086305 //integer
         );
     super.initState();
@@ -38,23 +135,42 @@ class _HomePageState extends State<HomePage> {
           SizedBox(
             width: 350,
             child: ListView.builder(
-              itemCount: 10,
+              itemCount: conversa.length,
               itemBuilder: (context, index) {
                 return Card(
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Column(
-                          children: [
-                            Text("$index"),
-                            Text("$index"),
-                            Text("$index"),
-                          ],
-                        ),
-                        Text("$index"),
-                      ],
+                  child: InkWell(
+                    onTap: () {
+                      setState(() {
+                        chatAberto = true;
+                        conversasSelecionadas = index;
+                      });
+                    },
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                conversa[index].nome,
+                                style: const TextStyle(
+                                    fontWeight: FontWeight.bold, fontSize: 15),
+                              ),
+                              const SizedBox(height: 8),
+                              Text(
+                                conversa[index].ultimaMensagem,
+                                style: const TextStyle(
+                                    fontStyle: FontStyle.italic),
+                              ),
+                              const SizedBox(height: 8),
+                              Text("status: ${conversa[index].status}"),
+                            ],
+                          ),
+                          Text("${conversa[index].hora}"),
+                        ],
+                      ),
                     ),
                   ),
                 );
@@ -62,52 +178,80 @@ class _HomePageState extends State<HomePage> {
             ),
           ),
           Expanded(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.end,
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: <Widget>[
-                ListView.builder(
-                  shrinkWrap: true,
-                  itemCount: conversa.length,
-                  itemBuilder: (context, index) {
-                    return Align(
-                        alignment: index.isEven
-                            ? Alignment.centerLeft
-                            : Alignment.centerRight,
-                        child: Padding(
-                          padding: const EdgeInsets.all(10.0),
-                          child: Card(
-                              shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(18.0)),
+            child: Visibility(
+              visible: chatAberto,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  IconButton(
+                    onPressed: () {
+                      setState(() {
+                        chatAberto = false;
+                      });
+                    },
+                    icon: const Icon(Icons.close),
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: <Widget>[
+                      ListView.builder(
+                        shrinkWrap: true,
+                        itemCount:
+                            conversa[conversasSelecionadas!].chats!.length,
+                        itemBuilder: (context, index) {
+                          return Align(
+                              alignment: index.isEven
+                                  ? Alignment.centerLeft
+                                  : Alignment.centerRight,
                               child: Padding(
                                 padding: const EdgeInsets.all(10.0),
-                                child: Text(conversa[index]),
-                              )),
-                        ));
-                  },
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: TextFormField(
-                    textInputAction: TextInputAction.newline,
-                    controller: mensagemController,
-                    decoration: InputDecoration(
-                        border: const OutlineInputBorder(
-                          borderRadius: BorderRadius.all(
-                            Radius.circular(100),
-                          ),
+                                child: Card(
+                                    shape: RoundedRectangleBorder(
+                                        borderRadius:
+                                            BorderRadius.circular(18.0)),
+                                    child: Padding(
+                                      padding: const EdgeInsets.all(10.0),
+                                      child: Text(
+                                          conversa[conversasSelecionadas!]
+                                              .chats![index]
+                                              .mensagem),
+                                    )),
+                              ));
+                        },
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: TextFormField(
+                          textInputAction: TextInputAction.newline,
+                          controller: mensagemController,
+                          decoration: InputDecoration(
+                              border: const OutlineInputBorder(
+                                borderRadius: BorderRadius.all(
+                                  Radius.circular(100),
+                                ),
+                              ),
+                              hintText: 'Mensagem',
+                              suffixIcon: IconButton(
+                                  onPressed: () {
+                                    setState(() {
+                                      chat.add(mensagemController.text);
+                                      // final reuturn =
+                                      //     await whatsapp.messagesTemplate(
+
+                                      //         to: 5544988683003,
+                                      //         templateName: conversa);
+                                      // print(reuturn);
+                                    });
+                                  },
+                                  icon: const Icon(Icons.send))),
                         ),
-                        hintText: 'Mensagem',
-                        suffixIcon: IconButton(
-                            onPressed: () {
-                              setState(() {
-                                conversa.add(mensagemController.text);
-                              });
-                            },
-                            icon: const Icon(Icons.send))),
+                      ),
+                    ],
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ],
@@ -115,7 +259,3 @@ class _HomePageState extends State<HomePage> {
     );
   }
 }
- 
-          //       final reuturn = await whatsapp.messagesTemplate(
-          //           to: 5544988683003, templateName: "hello_world");
-          //       print(reuturn);
